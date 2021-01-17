f71654398e98497239ee0222d71d3c3de24981a7
Richard Specter Liu <richardliu0502@gmail.com>
2021-01-17 16:55:56 +0800

    Add new file

---
+++ hi.py
@@ -0,0 +1,7 @@
+name = input('Wie heißt du:')
+print('Glad you speak German,', name)
+weight = input('How damn heavy are you:')
+print(weight, ',', 'you are as heavy as you look')
+height = input('Aight, how tall are you:')
+print(height, 'man, you are really tall')
+
